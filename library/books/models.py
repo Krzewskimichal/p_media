9dc6e33a1fe25145e29cf4938e6ec3473c053c4f
Krzewskimichal <koksawp@o2.pl>
2021-02-09 11:51:10 +0100

    create delete image file on delete record method

--- library/books/models.py
+++ library/books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.dispatch import receiver
 
+import os
 
 class Publisher(models.Model):
     name = models.CharField(max_length=128)
@@ -23,7 +25,39 @@ class Book(models.Model):
     author = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher, null=True, on_delete=models.SET_NULL)
     pages_num = models.IntegerField()
-    cover_image = models.ImageField()
+    cover_image = models.ImageField(upload_to='cover_images/')
 
     def __str__(self):
         return self.title
+
+
+@receiver(models.signals.post_delete, sender=Book)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.cover_image:
+        if os.path.isfile(instance.cover_image.path):
+            os.remove(instance.cover_image.path)
+
+
+@receiver(models.signals.pre_save, sender=Book)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = sender.objects.get(pk=instance.pk).cover_image
+    except sender.DoesNotExist:
+        return False
+
+    new_file = instance.cover_image
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
--- library/books/views.py
+++ library/books/views.py
@@ -19,6 +19,14 @@ class BooksModelViewSet(ModelViewSet):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def patch(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = BooksSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk, format=None):
         book = self.get_object(pk)
         book.delete()
@@ -29,7 +37,41 @@ class AuthorModelViewSet(ModelViewSet):
     serializer_class = AuthorsSerializer
     queryset = Author.objects.all()
 
+    def put(self, request, pk, format=None):
+        author = self.get_object(pk)
+        serializer = BooksSerializer(author, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        author = self.get_object(pk)
+        author.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class PublisherModelViewSet(ModelViewSet):
     serializer_class = PublishersSerializer
     queryset = Publisher.objects.all()
+
+    def put(self, request, pk, format=None):
+        publisher = self.get_object(pk)
+        serializer = BooksSerializer(publisher, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        publisher = self.get_object(pk)
+        serializer = BooksSerializer(publisher, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        publisher = self.get_object(pk)
+        publisher.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
